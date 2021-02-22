02af1daee60cd6578fe10fa1531b742bf2b6ad9a
Michael Abt <michael@webwelten.com>
2021-02-22 20:24:05 +0100

    refactor: add purge css plugin and use older parcel version

---
+++ purgecss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "content": [
+      "./app/index.html",
+      "./app/scripts/app.js",
+      "./app/scripts/vendor/foundation.js"
+    ],
+    "css": [
+      "./app/styles/foundation.min.css",
+    ],
+    "whitelist": [
+      "flatpickr-day"
+    ],
+    rejected: true
+}
